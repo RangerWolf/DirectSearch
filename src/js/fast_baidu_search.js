d0ad90fb09d2a0e8eff2b98853e908df2736a7a1
Wenjun Yang (QA-CN) <wenjun_yang@tw.trendnet.org>
2016-04-08 12:31:33 +0800

    fix bug: 误用了//协议

--- src/js/fast_baidu_search.js
+++ src/js/fast_baidu_search.js
@@ -127,7 +127,7 @@ function replaceLink(title) {
 				// 如果url没有被截断并且非百度自有域名
 				// 直接使用完整的url来替换
 				//console.log("%%" + showUrlTxt)
-				originLink.href = "//" + showUrlTxt
+				originLink.href = "http://" + showUrlTxt
 				// originLink.style.color = 'orange'
 			} 
 			else if(showUrlTxt.startsWith("zhidao.baidu.com") ){
