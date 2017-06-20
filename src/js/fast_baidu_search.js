97cf939e53dcb64418bd7cbfa28ed5d80e95cf35
Wenjun Yang (QA-CN) <wenjun_yang@tw.trendnet.org>
2017-06-20 20:37:43 +0800

    partially fix baidu issues

--- src/js/fast_baidu_search.js
+++ src/js/fast_baidu_search.js
@@ -95,125 +95,99 @@ function replaceLink(title) {
 		ajax_page_link = ajax_page_link + "&rn=50"
 
 	console.log("source page:" + ajax_page_link)
-	$.ajax({
-  		url: ajax_page_link,
-  		context: document.body
-	}).done(function(data) {
-		direct_links = $(data).find("td.f a[href]")
+	directResultArr = new Array();
 
-	  	directResultArr = new Array(direct_links.length / 2);
+	result_div = document.querySelectorAll('#content_left div.c-container')
 
-	  	for(var i = 0; i < direct_links.length; i+=2) {
-
-	  		tmpHref = direct_links[i].href
-	  		tmpText = direct_links[i].text
-	  		tmpText = $.trim(tmpText)
-			
-			// 此处将搜索结果的标题以及最终url都放在key之中，
-			// 这样在使用的时候就可以根据域名进行简单过滤，
-			// 避免因为标题相同而造成错乱
-			// 但是还是无法避免如果同一个域名下的搜索结果标题也相同的情况
-			keyHref = ""
-			if(tmpHref.startsWith("http://"))
-				keyHref = tmpHref.replace("http://", "")
-			else if(tmpHref.startsWith("https://"))
-				keyHref = tmpHref.replace("https://", "")
-			key = tmpText + "_" + decodeURI(keyHref)
-	  		directResultArr[key] = tmpHref
-	  		
-	  	}
-
-		result_div = document.querySelectorAll('#content_left div.c-container')
-
+	
+	var directLinksCnt = 0
+	for(var i = 0; i < result_div.length; i++) {
 		
-	  	var directLinksCnt = 0
-	  	for(var i = 0; i < result_div.length; i++) {
-			
-			// 头部特殊标注的新闻 暂时无法处理 会误操作
-	  		if($(result_div[i]).hasClass("xpath-log"))
-	  			continue
+		// 头部特殊标注的新闻 暂时无法处理 会误操作
+		if($(result_div[i]).hasClass("xpath-log"))
+			continue
 
-			originLink = result_div[i].querySelector("h3 a")			
-			showUrl    = result_div[i].querySelector(".c-showurl")
-			showUrl_g  = result_div[i].querySelector(".g")
+		originLink = result_div[i].querySelector("h3 a")			
+		showUrl    = result_div[i].querySelector(".c-showurl")
+		showUrl_g  = result_div[i].querySelector(".g")
 
-			showUrlTxt = ""
-			// 暂时没有去解析内部的div.g里面的showurl
-			if(showUrl != null && showUrl != undefined) {
-				showUrlTxt = showUrl.text
-				if(showUrlTxt == undefined || showUrlTxt == null) {
-					showUrlTxt = showUrl.innerHTML
-				}
-			} else if(showUrl_g != null && showUrl_g != undefined) {
-				showUrlTxt = showUrl_g.textContent
-				if(showUrlTxt != null) {
-					// Sample: www.iqiyi.com/yinyue/201...  - 百度快照
-					showUrlTxt = showUrlTxt.replace("  - 百度快照", "")
-				}
+		showUrlTxt = ""
+		// 暂时没有去解析内部的div.g里面的showurl
+		if(showUrl != null && showUrl != undefined) {
+			showUrlTxt = showUrl.text
+			if(showUrlTxt == undefined || showUrlTxt == null) {
+				showUrlTxt = showUrl.innerHTML
 			}
-			
-			showUrlTxt = $.trim(showUrlTxt)
-			if(showUrlTxt == null || showUrlTxt == undefined || showUrlTxt.length == 0) 
-				continue
-			
-			if( showUrlTxt.indexOf("...") == -1 && showUrlTxt.indexOf("baidu.com") == -1 && showUrlTxt.indexOf(" ") == -1) {
-				// 如果url没有被截断并且非百度自有域名
-				// 2017-02-07 有的URL并不是真实的URL，比如：“www.xcar.com.cn 2017-02-07” 这种格式的就不能替换。 
-				// 直接使用完整的url来替换
-				//console.log("%%" + showUrlTxt)
-				if(showUrlTxt.indexOf("https://") == -1 && showUrlTxt.indexOf("http://") == -1)
-					originLink.href = "http://" + showUrlTxt
-				else {
-					originLink.href = showUrlTxt
-				}
-				// originLink.style.color = 'orange'
-			} 
+		} else if(showUrl_g != null && showUrl_g != undefined) {
+			showUrlTxt = showUrl_g.textContent
+			if(showUrlTxt != null) {
+				// Sample: www.iqiyi.com/yinyue/201...  - 百度快照
+				showUrlTxt = showUrlTxt.replace("  - 百度快照", "")
+			}
+		}
+		
+		showUrlTxt = $.trim(showUrlTxt)
+		if(showUrlTxt == null || showUrlTxt == undefined || showUrlTxt.length == 0) 
+			continue
+		
+		if( showUrlTxt.indexOf("...") == -1 && showUrlTxt.indexOf("baidu.com") == -1 && showUrlTxt.indexOf(" ") == -1) {
+			// 如果url没有被截断并且非百度自有域名
+			// 2017-02-07 有的URL并不是真实的URL，比如：“www.xcar.com.cn 2017-02-07” 这种格式的就不能替换。 
+			// 直接使用完整的url来替换
+			//console.log("%%" + showUrlTxt)
+			if(showUrlTxt.indexOf("https://") == -1 && showUrlTxt.indexOf("http://") == -1)
+				originLink.href = "http://" + showUrlTxt
 			else {
-				// 其他url 还是使用ajax result 替换
-				showUrlTxt = showUrlTxt.replace(/\.\.\..*/, "")
-				showUrlTxt = $.trim(showUrlTxt)
-				baiduDotComIndex = showUrlTxt.indexOf("baidu.com")
-				if(baiduDotComIndex > 0 ) {
-					showUrlTxt = showUrlTxt.substring(0, baiduDotComIndex + "baidu.com".length)
-				}
+				originLink.href = showUrlTxt
+			}
+			// originLink.style.color = 'orange'
+		} 
+		else {
+			// 其他url 还是使用ajax result 替换
+			showUrlTxt = showUrlTxt.replace(/\.\.\..*/, "")
+			showUrlTxt = $.trim(showUrlTxt)
+			baiduDotComIndex = showUrlTxt.indexOf("baidu.com")
+			if(baiduDotComIndex > 0 ) {
+				showUrlTxt = showUrlTxt.substring(0, baiduDotComIndex + "baidu.com".length)
+			}
 
-				// console.log("originLink == null:" + (originLink == null))
-				if(originLink == null) {
-					console.log("i=" + i)
-					continue;
-				}
-				tmpHref = originLink.href
-				tmpText = originLink.textContent
-				tmpText = $.trim(tmpText)
-				
-				tmpKey = tmpText + "_" + showUrlTxt
-				console.log("tmpKey=" + tmpKey)
-				
-				// 这里采用了一个不太高效的循环遍历，但是好在数据量很小
-				for(var resultKey in directResultArr) {
-					// console.log("result key:" + resultKey)
-					// console.log("target key:" + tmpKey)
+			// console.log("originLink == null:" + (originLink == null))
+			if(originLink == null) {
+				console.log("i=" + i)
+				continue;
+			}
+			tmpHref = originLink.href
+			tmpText = originLink.textContent
+			tmpText = $.trim(tmpText)
+			
+			tmpKey = tmpText + "_" + showUrlTxt
+			console.log("tmpKey=" + tmpKey)
+			
+			// 这里采用了一个不太高效的循环遍历，但是好在数据量很小
+			for(var resultKey in directResultArr) {
+				// console.log("result key:" + resultKey)
+				// console.log("target key:" + tmpKey)
 
-					if(resultKey.startsWith(tmpKey)) {					
-						originLink.href = directResultArr[resultKey]
-						// originLink.style.color = 'red'
-						directLinksCnt++
-						break;
-					}
+				if(resultKey.startsWith(tmpKey)) {					
+					originLink.href = directResultArr[resultKey]
+					// originLink.style.color = 'red'
+					directLinksCnt++
+					break;
 				}
 			}
-			
-			// 如果存在缩略图，则把缩略图里面的链接也替换掉
-			thumbnail_link = result_div[i].querySelector("a.c-img6")
-			if(thumbnail_link != null && thumbnail_link != undefined) {
-				console.log("图片链接替换..." + originLink.href)
-				thumbnail_link.href = originLink.href
-			}
-			
-	  	}
+		}
+		
+		// 如果存在缩略图，则把缩略图里面的链接也替换掉
+		thumbnail_link = result_div[i].querySelector("a.c-img6")
+		if(thumbnail_link != null && thumbnail_link != undefined) {
+			console.log("图片链接替换..." + originLink.href)
+			thumbnail_link.href = originLink.href
+		}
+		
+	}
 
-	  	console.log("direct links count:" + directLinksCnt)
-	  	$("div.nums").append($("<label style='color:green'>重定向已去除</label>"))
-	});
+	console.log("direct links count:" + directLinksCnt)
+	$("div.nums").append($("<label style='color:green'>重定向已部分去除</label>"))
+	
 
 }
